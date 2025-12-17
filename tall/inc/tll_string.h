d26957382449760bc34678cde85cc136db52084c
t0m45DEV <tomaslisazo12@gmail.com>
2025-12-17 16:39:02 -0300

    Commets for string functions

--- tall/inc/tll_string.h
+++ tall/inc/tll_string.h
@@ -17,17 +17,24 @@ void remove_redundant_spaces(char* string);
 void string_to_upper(char* string);
 
 /**
+ * Returns the ammount of lines in the given string
+ */
+int get_line_count(const char* string);
+
+/**
+ * Gets the next token of the given string delimited by the given delimiter, and updates found_cnt to the token index corresponding to the result token
  *
+ * Returns NULL if not token was found
  */
 char* next_token(char** string, char delimiter, char* token, int* found_cnt);
 
 /**
- *
+ * Same as next_token, but with entire lines
  */
 char* next_line(char** string, char* token, int* found_cnt);
 
 /**
- *
+ * Same as next_token, but with words
  */
 char* next_word(char** string, char* token, int* found_cnt);
 
