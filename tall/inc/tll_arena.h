84fd5695a54461a18618923fa768f5b302094845
t0m45DEV <t0m45dev@pm.me>
2026-06-14 19:27:19 -0300

    Memory arena manager

---
+++ tall/inc/tll_arena.h
@@ -0,0 +1,34 @@
+#ifndef _TLL_ARENA_H
+#define _TLL_ARENA_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+/**
+ * A memory chunk, like a big pool for data collection and fast creation.
+ */
+typedef struct tll_memory_arena {
+    uint8_t* capacity;
+    uint8_t* available;
+    uint8_t* space;
+    struct tll_memory_arena* next;
+} tll_memory_arena;
+
+/**
+ * Allocates the memory needed for a memory arena.
+ */
+tll_memory_arena* create_arena(void);
+
+/**
+ * Frees the memory used by the given arena.
+ */
+void free_arena(tll_memory_arena* arena);
+
+/**
+ * Adds the given element to the given memory arena and returns that arena pointer, unless there is no more space available in there.
+ *
+ * If there is no more space, it creates a new arena next to the given one, and returns the pointer to the new arena.
+ */
+tll_memory_arena* add_to_arena(tll_memory_arena* arena, void* element, size_t size);
+
+#endif
---
+++ tall/src/tll_arena.c
@@ -0,0 +1,54 @@
+#include "tll_arena.h"
+
+#include "tll_memory.h"
+
+#include <string.h>
+
+// The size of a given arena.
+#define ARENA_SIZE (1024 * 1024)
+
+tll_memory_arena* create_arena(void)
+{
+    tll_memory_arena* arena = reallocate(NULL, 0, sizeof(tll_memory_arena));
+
+    arena->next = NULL;
+    arena->space = reallocate(NULL, 0, ARENA_SIZE);
+    arena->available = arena->space;
+    arena->capacity = arena->space + ARENA_SIZE;
+
+    return arena;
+}
+
+void free_arena(tll_memory_arena* arena)
+{
+    tll_memory_arena* current = arena;
+
+    while (current != NULL)
+    {
+        tll_memory_arena* next = current->next;
+
+        current->capacity = 0;
+        current->next = NULL;
+        current->available = NULL;
+        current->space = reallocate(current->space, ARENA_SIZE, 0);
+
+        current = reallocate(current, sizeof(tll_memory_arena), 0);
+        current = next;
+    }
+}
+
+tll_memory_arena* add_to_arena(tll_memory_arena* arena, void* element, size_t size)
+{
+    tll_memory_arena* ret = arena;
+
+    if (arena->available + size > arena->capacity)
+    {
+        arena->next = create_arena();
+        ret = arena->next;
+    }
+    memcpy(ret->available, element, size);
+    ret->available += size;
+
+    return ret;
+}
+
