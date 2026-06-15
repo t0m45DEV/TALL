3e556f46b342d05cff984020cda023faf3fcea5d
t0m45DEV <t0m45dev@pm.me>
2026-06-14 22:14:33 -0300

    Better formats for reallocate

--- tall/src/tll_memory.c
+++ tall/src/tll_memory.c
@@ -1,5 +1,6 @@
 #include "tll_memory.h"
 
+#include <stdint.h>
 #include <string.h>
 #include <sysexits.h>
 #include <stdlib.h>
@@ -16,13 +17,13 @@ void* reallocate(void* pointer, size_t old_capacity, size_t new_capacity)
 
     if (result == NULL)
     {
-        printf("[FATAL ERROR] Tried to allocate %lu bytes of memory and failed.\n", new_capacity);
+        printf("[FATAL ERROR] Tried to allocate %zu bytes of memory and failed.\n", new_capacity);
         exit(EX_OSERR);
     }
 
     if (new_capacity > old_capacity)
     {
-        memset(result + old_capacity, 0, new_capacity - old_capacity);
+        memset((uint8_t*) result + old_capacity, 0, new_capacity - old_capacity);
     }
     return result;
 }
